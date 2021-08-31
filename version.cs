1f3181f59334bacfa3447bfe0f1f059447c6cbc1
Brad Robinson <contact@toptensoftware.com>
2021-08-31 10:15:16 +1000

    b0.1.102

--- version.cs
+++ version.cs
@@ -3,13 +3,13 @@
 using System;
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2021 Topten Software. All Rights Reserved")]
-[assembly: AssemblyVersion("0.1.101")]
-[assembly: AssemblyFileVersion("0.1.101")]
+[assembly: AssemblyVersion("0.1.102")]
+[assembly: AssemblyFileVersion("0.1.102")]
 [assembly: AssemblyCompany("Topten Software")]
 [assembly: AssemblyProduct("nvpatch")]
 
 static class BuildInfo
 {
-	public static DateTime Date = new DateTime(2021, 8, 31, 0, 12, 55, DateTimeKind.Utc);
+	public static DateTime Date = new DateTime(2021, 8, 31, 0, 15, 7, DateTimeKind.Utc);
 }
 	
